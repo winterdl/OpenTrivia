0292d91d2a3343869ec91db5770c3c7ce5e40f6c
Anushan Lingam <5958017+AnushanLingam@users.noreply.github.com>
2018-11-19 18:49:51 +0000

    Reworked Joining and Disconnecting
    * Minor node module updates

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const manager = require('./modules/gameManager.js');
 
 const app = express();
-
 const clientPath = __dirname + "/client";
+app.use(express.static(clientPath));
 console.log("Serving files to client " + clientPath);
 
-app.use(express.static(clientPath));
 
 const server = http.createServer(app);
 const io = socketio(server);
@@ -16,6 +16,34 @@ const io = socketio(server);
 let games = {};
 io.on('connection', function(socket){
     console.log(socket.id + " connected!")
+
+    socket.on("verifyRoom", function(data){
+        if(!manager.doesExist(data, games)) {
+            let game = manager.createGame(socket.id, data);
+            games[data] = game;
+            console.log("New Game Created: " + game.toString());
+            io.sockets.connected[socket.id].emit("roomAvailable", true);
+        } else (io.sockets.connected[socket.id].emit("roomAvailable", false))
+    });
+
+
+    
+
+    socket.on("disconnect", function(data){
+        let query = manager.getClientType(socket.id, games);
+        if(query == undefined) {
+            console.log(socket.id + " " + "disconnected from join screen.");
+        } else if(query[0] == "host") {
+            delete games[query[1]];
+            io.in(query[1]).emit("hostDisconnected");
+            console.log("Host " + socket.id + " disconnected.\n" + "Room " + query[1] + " has been deleted!");
+        } else if(query[0] == "player") {
+            let response = manager.removePlayer(socket.id, games[query[1]].playerList);
+            games[query[1]].playerList = response[0];
+            io.in(query[1]).emit("playerDisconnected", response[1]);
+            console.log(response[1] + " disconnected and removed from room " + query[1]);
+        }
+    });
 });
 
 
